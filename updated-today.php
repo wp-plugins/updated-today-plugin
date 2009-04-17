db5566f0532b243efb007da3876155b51f0727ec
cklosows <cklosows@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-17 19:03:48 +0000

    Checking in version 1.8.1
    git-svn-id: https://plugins.svn.wordpress.org/updated-today-plugin/tags/1.8.1@111326 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- updated-today.php
+++ updated-today.php
@@ -3,7 +3,7 @@
 Plugin Name: Updated Today Banner
 Plugin URI: http://www.chriskdesigns.com/updated-today/
 Description: This plug-in provides a banner in the upper left corner of the page that says "updated today" if your Wordpress Blog has been updated today.
-Version: 1.6.1
+Version: 1.8.1
 Author: Chris Klosowski
 Author URI: http://www.chriskdesigns.com/
 License: GPL
@@ -20,23 +20,15 @@ $conf_use_pngfix = true;
 $banneradded = null;
 
 add_action('wp_head', 'ck_wp_head');
-if (!$conf_manual_placement) {
-    add_action('wp_footer', 'ck_wp_footer');
-}
+add_action('wp_footer', 'ck_wp_footer');
 
 function ck_wp_head ()
 {
     global $conf_manual_style, $conf_use_pngfix;
     if (!$conf_manual_style) {
-        echo '<style type="text/css" media="screen">#updated {
-    position: absolute;
-    display: block;
-            top: 0px;
-    left: 0px;
-    height: 120px;
-    width: 120px;
-    z-index: 100;
-        } </style>';
+    echo '<style type="text/css" media="screen">
+    #updated { clear: both; position: absolute; display: block; top: 0; left: 0; height: 120px; width: 120px; z-index: 100; } 
+    #updated img { padding: 0; margin: 0; }</style>';
     }
     if ($conf_use_pngfix) {
         echo '<!--[if lt IE 7]>
@@ -47,9 +39,10 @@ function ck_wp_head ()
 
 function updated_banner()
 {
+    global $table_prefix;
     $today = date("Y-m-d");
     $status = 'publish';
-    $query = "SELECT post_date, id FROM wp_posts WHERE wp_posts.post_modified LIKE '".$today."%' AND wp_posts.post_status='publish'";
+    $query = "SELECT post_date, id FROM wp_posts WHERE (".$table_prefix."posts.post_date LIKE '".$today."%' OR ".$table_prefix."posts.post_modified LIKE '".$today."%') AND ".$table_prefix."posts.post_status='publish'";
     $results = mysql_query($query);
     $num_results = mysql_num_rows($results);
     if ($num_results > 0) {
